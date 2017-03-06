a46009e6aaf520b4fd28f8601537326733e289bf
VictorP <vphillips@wlapcs.org>
2017-03-06 20:20:26 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,47 @@
+function setup(){
+   createCanvas(600,400 ); 
+   background(100,100,100);
+}
+
+function draw(){
+    stroke(67,89,45);
+    fill(97,600,544);
+    rect(73,30,203,203);
+    
+    line(35,80,400,200);
+    
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(200,100,250,250);
+    
+    stroke(0,0,0);
+    fill(0,0,255);
+    ellipse(50,180,80,80);
+    
+    stroke(5,5,5);
+    fill(0,255,0);
+    ellipse(40,58,85,25);
+    
+    line(46,68,200,403);
+    
+    stroke(23,45,56,67);
+    fill(0,68,25);
+    ellipse(57,300,57,65);
+    
+    line(600,710,645);
+    
+    stroke(99,73,33);
+    fill(12,0,0);
+    ellipse(200,814,34,68);
+    
+    line(50,20,400,20);
+
+    line(500,20,500,300);
+    
+    
+}
+
+//draw me 2 ellipse
+//draw me 2 rectangles
+//draw me 4 lines (1 with a positive slop,1 with a neg slope,)
+//1 with a slope of 0, and 1 with a slope  of underfine.
